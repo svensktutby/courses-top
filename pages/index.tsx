6c954ebdf5c4f82cb02abb7b87329220ff186976
svensktutby <svensk.tut.by@gmail.com>
2021-07-20 13:22:52 +0300

    feat: add Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import { Htag } from '../components';
+
 export default function Home(): JSX.Element {
-  return <div>Main page</div>;
+  return (
+    <>
+      <Htag tag="h1">Htag</Htag>
+    </>
+  );
 }
