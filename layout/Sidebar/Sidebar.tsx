613cb9d7644b98a78609e5dae32d1015cd79c565
svensktutby <svensk.tut.by@gmail.com>
2021-07-28 19:09:22 +0300

    feat: markup Sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,15 @@ import cn from 'classnames';
 import styles from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
 import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <aside {...props}>
+    <aside className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+
+      <div>поиск</div>
+
       <Menu />
     </aside>
   );
